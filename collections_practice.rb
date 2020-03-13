32f301760343fa168f28a95e6cb8e45945779dd3
Adam Priest <sepone229@gmail.com>
2020-03-13 01:02:57 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -13,3 +13,12 @@ end
 def sort_array_char_count(array)
   array.sort {|a,b| a.length <=> b.length}
 end
+
+def swap_elements_from_to(array, index, destination_index)
+  a
+end
+
+def reverse_array(array)
+  array.reverse do |a,b|
+    a <=> b
+end
