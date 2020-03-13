e16dec6608dea884da1a8d23a1bc7e67b4c82192
Adam Priest <sepone229@gmail.com>
2020-03-13 02:08:27 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,54 @@
+def sort_array_asc(array)
+  array.sort
+end
+def sort_array_desc(array)
+  array.sort.reverse do |a,b|
+    a <=> b
+  end
+end
+def sort_array_char_count(array)
+  array.sort {|a,b| a.length <=> b.length}
+end
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  array
+end
+def reverse_array(array)
+  array.reverse
+end
+def kesha_maker(array)
+  array.collect do |word|
+    word[2] = "$"
+    word
+  end
+end
+def find_a(array)
+  array.select do |word|
+    word.chr == "a"
+  end
+end
+def sum_array(array)
+  array.inject do |sum, num|
+    sum + num
+  end
+end
+def add_s(array)
+  array.each_with_index.collect do |element, index|
+    if index == 1
+      element
+    else element[element.length] = "s"
+      element
+    end
+  end
+end
+
+
+
+
+
+
+
+
+
+
 
