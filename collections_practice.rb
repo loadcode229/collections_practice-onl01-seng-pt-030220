f65918f3921c90c7f304cc014a8c8c0ec994c556
Adam Priest <sepone229@gmail.com>
2020-03-13 01:37:58 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -39,7 +39,15 @@ def sum_array(array)
 end
 
 def add_s(array)
-  
+  array.each_with_index.collect do |element, index|
+    if index == 1
+      element
+    else
+      element[element.length] = "s"
+      element
+    end
+  end
+end
 end
 
 
